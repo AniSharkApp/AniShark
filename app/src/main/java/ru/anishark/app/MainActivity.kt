3035ec7c4a4f2c56df5f26b2703d7e9d5f8e5131
MiddleShamil <dmitriy_abdullaev@bk.ru>
2024-05-01 21:18:18 +0300

    feature: added main layout, colors to light theme and some icons

--- app/src/main/java/ru/anishark/app/MainActivity.kt
+++ app/src/main/java/ru/anishark/app/MainActivity.kt
@@ -1,20 +1,48 @@
 package ru.anishark.app
 
+import androidx.fragment.app.Fragment
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.anishark.app.fragments.BookmarkFragment
+import ru.anishark.app.fragments.CatalogFragment
+import ru.anishark.app.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setSupportActionBar(findViewById(R.id.top_app_bar))
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        loadFragment(HomeFragment())
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        bottomNav.setOnItemSelectedListener { fragment ->
+            when(fragment.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.catalog -> {
+                    loadFragment(CatalogFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(BookmarkFragment())
+                    true
+                }
+            }
         }
     }
+
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
 }
+
