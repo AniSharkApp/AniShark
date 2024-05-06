415147d323c2816220907dc474730f4864ac841c
Alexander Ereshkin <xcode111@mail.ru>
2024-05-07 01:00:37 +0300

    Add "Show more" card

--- app/src/main/java/ru/anishark/app/feature/home/recycler/HomeAnimeListAdapter.kt
+++ app/src/main/java/ru/anishark/app/feature/home/recycler/HomeAnimeListAdapter.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import ru.anishark.app.R
 import ru.anishark.app.databinding.CardAnimeHomeBinding
+import ru.anishark.app.databinding.CardWatchMoreHomeBinding
 
 class AnimeModel(
     val name: String,
@@ -17,6 +18,9 @@ class AnimeModel(
 class HomeAnimeListAdapter(
     val data: List<AnimeModel> = MutableList(10) { it -> AnimeModel(name = "Anime ${it + 1}", episodesCount = it) }
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class MoreViewHolder(
+        val binding: CardWatchMoreHomeBinding
+    ) : RecyclerView.ViewHolder(binding.root)
     class AnimeViewHolder(
         val binding: CardAnimeHomeBinding
     ) : RecyclerView.ViewHolder(binding.root) {
@@ -29,20 +33,33 @@ class HomeAnimeListAdapter(
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (position == data.size) WATCH_MORE_CARD_TYPE else ANIME_CARD_TYPE
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerView.ViewHolder {
+        if (viewType == WATCH_MORE_CARD_TYPE) {
+            val watchMoreBinding = CardWatchMoreHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return MoreViewHolder(watchMoreBinding)
+        }
         val animeCardViewBinding =
             CardAnimeHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return AnimeViewHolder(animeCardViewBinding)
     }
 
-    override fun getItemCount(): Int = data.size
+    override fun getItemCount(): Int = data.size + 1
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is AnimeViewHolder) {
             holder.bind(data[position])
         }
+    }
+
+    companion object {
+        const val ANIME_CARD_TYPE = 1
+        const val WATCH_MORE_CARD_TYPE = 2
     }
 }
