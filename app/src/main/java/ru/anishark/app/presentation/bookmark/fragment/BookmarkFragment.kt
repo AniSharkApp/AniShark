7b531868e139d4489ddd0f10eaa14e89fe237900
Alexander Ereshkin <xcode111@mail.ru>
2024-05-14 19:45:48 +0300

    Remove useless newInstance method in BookmarkFragment and add annotation to it.

--- app/src/main/java/ru/anishark/app/presentation/bookmark/fragment/BookmarkFragment.kt
+++ app/src/main/java/ru/anishark/app/presentation/bookmark/fragment/BookmarkFragment.kt
@@ -5,22 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import ru.anishark.app.R
 import javax.inject.Inject
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BookmarkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BookmarkFragment @Inject constructor(): Fragment() {
+@AndroidEntryPoint
+class BookmarkFragment @Inject constructor() : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -42,22 +32,7 @@ class BookmarkFragment @Inject constructor(): Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookmarkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookmarkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val ARG_PARAM1 = "param1"
+        const val ARG_PARAM2 = "param2"
     }
 }
