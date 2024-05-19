dda014c71445f977b137d12917afe6f8bb54f730
Alexander Ereshkin <xcode111@mail.ru>
2024-05-20 01:37:05 +0300

    Remove krinzhanul

--- app/src/main/java/ru/anishark/app/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/anishark/app/presentation/main/MainActivity.kt
@@ -14,20 +14,10 @@ import ru.anishark.app.databinding.ActivityMainBinding
 import ru.anishark.app.presentation.bookmark.fragment.BookmarkFragment
 import ru.anishark.app.presentation.catalog.fragment.CatalogFragment
 import ru.anishark.app.presentation.home.fragment.HomeFragment
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    @Inject
-    lateinit var homeFragment: HomeFragment
-
-    @Inject
-    lateinit var catalogFragment: CatalogFragment
-
-    @Inject
-    lateinit var bookmarkFragment: BookmarkFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -44,22 +34,22 @@ class MainActivity : AppCompatActivity() {
 //            insets
 //        }
 
-        loadFragment(homeFragment)
+        loadFragment(HomeFragment::class.java)
         // TODO: сделать реализацию navigation по человечески
         binding.bottomNavBar.setOnItemSelectedListener { fragment ->
             when (fragment.itemId) {
                 R.id.home -> {
-                    loadFragment(homeFragment)
+                    loadFragment(HomeFragment::class.java)
                     true
                 }
 
                 R.id.catalog -> {
-                    loadFragment(catalogFragment)
+                    loadFragment(CatalogFragment::class.java)
                     true
                 }
 
                 else -> {
-                    loadFragment(bookmarkFragment)
+                    loadFragment(BookmarkFragment::class.java)
                     true
                 }
             }
@@ -83,9 +73,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadFragment(fragment: Fragment) {
+    private fun <T: Fragment> loadFragment(fragment: Class<out T>) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(binding.container.id, fragment)
+        transaction.replace(binding.container.id, fragment, null)
         transaction.commit()
     }
 }
