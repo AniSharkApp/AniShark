3545a9fa52eaf979ed43f8d417e9998db34f096f
Alexander Ereshkin <xcode111@mail.ru>
2024-05-20 13:29:33 +0300

    Add network API (3/?)

--- app/src/main/java/ru/anishark/app/data/remote/api/SeasonsService.kt
+++ app/src/main/java/ru/anishark/app/data/remote/api/SeasonsService.kt
@@ -1,11 +1,11 @@
 package ru.anishark.app.data.remote.api
 
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import ru.anishark.app.data.remote.RemoteConstants.SEASONS_NOW_PATH
 import ru.anishark.app.data.remote.dto.response.SeasonsNowDTO
 
 interface SeasonsService {
     @GET(SEASONS_NOW_PATH)
-    fun getSeasonsNow(): Observable<SeasonsNowDTO>
+    fun getSeasonsNow(): Single<SeasonsNowDTO>
 }
--- app/src/main/java/ru/anishark/app/data/remote/api/TopService.kt
+++ app/src/main/java/ru/anishark/app/data/remote/api/TopService.kt
@@ -1,11 +1,11 @@
 package ru.anishark.app.data.remote.api
 
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import ru.anishark.app.data.remote.RemoteConstants.TOP_ANIME_PATH
 import ru.anishark.app.data.remote.dto.response.TopAnimeDTO
 
 interface TopService {
     @GET(TOP_ANIME_PATH)
-    fun getTopAnime(): Observable<TopAnimeDTO>
+    fun getTopAnime(): Single<TopAnimeDTO>
 }
