999b6c8368670c10bb658e3cc063e64cbe635337
MiddleShamil <dmitriy_abdullaev@bk.ru>
2024-06-10 14:34:22 +0300

    feature: added functions to viewmodel, using rxjava

---
+++ app/src/main/java/ru/anishark/app/domain/model/BookmarkModel.kt
@@ -0,0 +1,7 @@
+package ru.anishark.app.domain.model
+
+data class BookmarkModel(
+    val animeId: Int,
+    val image: String, // TODO: переделать для картинок
+    val title: String
+)
--- app/src/main/java/ru/anishark/app/presentation/bookmark/viewmodel/BookmarkViewModel.kt
+++ app/src/main/java/ru/anishark/app/presentation/bookmark/viewmodel/BookmarkViewModel.kt
@@ -3,18 +3,31 @@ package ru.anishark.app.presentation.bookmark.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import ru.anishark.app.data.db.items.BookmarkAnimeEntity
+import ru.anishark.app.domain.usecase.DeleteBookmarkCatalogUseCase
 import ru.anishark.app.domain.usecase.GetAllBookmarksCatalogUseCase
+import ru.anishark.app.domain.usecase.GetOneBookmarkCatalogUseCase
+import ru.anishark.app.domain.usecase.InsertBookmarkCatalogUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class BookmarkViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val bookmarksUseCase: GetAllBookmarksCatalogUseCase
+    private val allBookmarksUseCase: GetAllBookmarksCatalogUseCase,
+    private val oneBookmarkUseCase: GetOneBookmarkCatalogUseCase,
+    private val insertBookmarkUseCase: InsertBookmarkCatalogUseCase,
+    private val deleteBookmarkUseCase: DeleteBookmarkCatalogUseCase
 ): ViewModel() {
+    private val _bookmarksState = BehaviorSubject.fromFuture(allBookmarksUseCase().toFuture())
+    val bookmarks get() = _bookmarksState.hide()
 
+    fun insertBookmark(anime: BookmarkAnimeEntity) = insertBookmarkUseCase(anime)
 
+    fun deleteBookmark(anime: BookmarkAnimeEntity) = deleteBookmarkUseCase(anime)
 
-    override fun onCleared() {
-        super.onCleared()
-    }
+    fun getBookmark(animeId: Int) = oneBookmarkUseCase(animeId).toFuture().get()
+
 }
